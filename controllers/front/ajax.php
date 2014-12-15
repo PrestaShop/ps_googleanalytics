72f4c7d79eafdfd321acfe60a4d83e36f4de8896
Daniel <daniel@prestaworks.se>
2014-12-15 18:43:28 +0800

    Update ajax.php

--- controllers/front/ajax.php
+++ controllers/front/ajax.php
@@ -24,7 +24,7 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
-class GanalyticsajaxModuleFrontController extends ModuleFrontController
+class GanalyticsAjaxModuleFrontController extends ModuleFrontController
 {
 	/*
 	 * @see FrontController::initContent()
