d2fba61dddc927e20236432c0c50fbd751565772
kermes <kermes@gmail.com>
2014-12-15 17:05:04 +0100

    [-] MO: ganalytics: screwing with bounce rate
    Some automatic events (product page detail view) are being recorded as user interactions so the bounce rate was totally screwed up

--- views/js/GoogleAnalyticActionLib.js
+++ views/js/GoogleAnalyticActionLib.js
@@ -76,7 +76,7 @@ var GoogleAnalyticEnhancedECommerce = {
 	addProductDetailView: function(Product) {
 		this.add(Product);
 		ga('ec:setAction', 'detail');
-		ga('send', 'event', 'UX', 'click', 'Product Detail View');
+		ga('send', 'event', 'UX', 'detail', 'Product Detail View',{'nonInteraction': 1});
 	},
 
 	addToCart: function(Product) {
@@ -144,6 +144,7 @@ var GoogleAnalyticEnhancedECommerce = {
 		});
 
 		ga('send', 'event', 'Product Click', 'click', Product.list, {
+			'nonInteraction': 1,
 			'hitCallback': function() {
 				return !ga.loaded;
 			}
