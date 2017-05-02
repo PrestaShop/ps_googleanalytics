c4e7954dcff17713e6b5a5162bc09b14f048679c
cedricfontaine <cfontaine@spidmail.net>
2017-05-02 12:03:13 -0400

    Migration for 1.7 - release 3.0.0

--- ps_googleanalytics.php
+++ ps_googleanalytics.php
@@ -39,7 +39,7 @@ class Ps_Googleanalytics extends Module
 	{
 		$this->name = 'ps_googleanalytics';
 		$this->tab = 'analytics_stats';
-		$this->version = '2.3.4';
+		$this->version = '3.0.0';
 		$this->author = 'PrestaShop';
 		$this->module_key = 'fd2aaefea84ac1bb512e6f1878d990b8';
 		$this->bootstrap = true;
