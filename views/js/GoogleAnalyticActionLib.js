5a57fa157ae32fc71cd16d66128486bb48ac4eaa
apacios <adrien.pacios@prestashop.com>
2020-05-05 15:42:29 +0200

    New date header 2007 to 2020

--- views/js/GoogleAnalyticActionLib.js
+++ views/js/GoogleAnalyticActionLib.js
@@ -1,5 +1,5 @@
 /**
- * 2007-2015 PrestaShop
+ * 2007-2020 PrestaShop
  *
  * NOTICE OF LICENSE
  *
@@ -18,7 +18,7 @@
  * needs please refer to http://www.prestashop.com for more information.
  *
  *  @author    PrestaShop SA <contact@prestashop.com>
- *  @copyright 2007-2015 PrestaShop SA
+ *  @copyright 2007-2020 PrestaShop SA
  *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  *  International Registered Trademark & Property of PrestaShop SA
  **/
@@ -175,7 +175,7 @@ var GoogleAnalyticEnhancedECommerce = {
 		});
 		ga('send', 'pageview');
 	},
-	
+
 	addCheckoutOption: function(Step,Option) {
 		ga('ec:setAction', 'checkout_option', {
 			'step': Step,
