d8bd75fcc22a1439dc686968241e52ee98997afe
Matthieu Rolland <matthieurolland@MacBook-Pro-de-Matthieu.local>
2024-02-28 16:55:48 +0100

    fix condition in order validation hook

--- classes/Hook/HookActionValidateOrder.php
+++ classes/Hook/HookActionValidateOrder.php
@@ -55,7 +55,8 @@ class HookActionValidateOrder implements HookInterface
     {
         // Check if we are creating backoffice order, we are only launching this hook when creating backoffice order
         // For FO purposes, we use displayOrderConfirmation.
-        if (empty($this->context->controller->controller_name) || ($this->context->controller->controller_name != 'AdminOrders' && $this->context->controller->controller_name != 'Admin')) {
+        if (empty($this->context->controller->controller_name)
+        || !in_array($this->context->controller->controller_name, ['AdminOrders', 'Admin'])) {
             return;
         }
 
